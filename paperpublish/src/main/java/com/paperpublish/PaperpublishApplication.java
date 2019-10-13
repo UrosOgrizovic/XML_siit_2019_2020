d7899d2f5aeb78c934099189f3d3441fdab3d07c
Uros Ogrizovic <uros.ogrizovic671@gmail.com>
2019-10-13 17:26:54 +0200

    Initial commit

---
+++ paperpublish/src/main/java/com/paperpublish/PaperpublishApplication.java
@@ -0,0 +1,13 @@
+package com.paperpublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperpublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperpublishApplication.class, args);
+	}
+
+}
---
+++ paperpublish/src/test/java/com/paperpublish/PaperpublishApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paperpublish;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaperpublishApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
