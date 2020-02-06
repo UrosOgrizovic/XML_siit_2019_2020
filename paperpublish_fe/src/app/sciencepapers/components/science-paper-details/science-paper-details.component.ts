7adb7a90111a6114d3e0cbcf538743a17aa9a8ad
Veljko Vucenic <vmvucenic@live.com>
2020-02-07 00:21:42 +0100

    added short video

--- paperpublish_fe/src/app/sciencepapers/components/science-paper-details/science-paper-details.component.ts
+++ paperpublish_fe/src/app/sciencepapers/components/science-paper-details/science-paper-details.component.ts
@@ -46,6 +46,56 @@ export class SciencePaperDetailsComponent implements OnInit {
     } else {
       document.getElementById('metadataTable').style.visibility = 'hidden';
     }
-    
+  }
+
+  
+  downloadPDF(id: number) {
+    this.sciencePapersService.getPDF(id).subscribe(result => {
+      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveOrOpenBlob(result);
+        return;
+      }
+      // For other browsers:
+      // Create a link pointing to the ObjectURL containing the blob.
+      const data = window.URL.createObjectURL(result);
+
+      var link = document.createElement('a');
+      link.href = data;
+      this.sciencePapersService.getPaperTitleById(id).subscribe(title => { 
+        link.download = title + '.pdf';
+        // this is necessary as link.click() does not work on the latest firefox
+        link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
+
+        setTimeout(function () {
+            // For Firefox it is necessary to delay revoking the ObjectURL
+            window.URL.revokeObjectURL(data);
+        }, 100);
+      });
+    });
+  }
+
+  downloadHTML(id: number) {
+    this.sciencePapersService.getHTML(id, 'blob').subscribe(result => {
+      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveOrOpenBlob(result);
+        return;
+      } 
+      // For other browsers: 
+      // Create a link pointing to the ObjectURL containing the blob.
+      const data = window.URL.createObjectURL(result);
+
+      var link = document.createElement('a');
+      link.href = data;
+      this.sciencePapersService.getPaperTitleById(id).subscribe(title => {
+        link.download = title + '.html';
+        // this is necessary as link.click() does not work on the latest firefox
+        link.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
+
+        setTimeout(function () {
+          // For Firefox it is necessary to delay revoking the ObjectURL
+          window.URL.revokeObjectURL(data);
+        }, 100);
+      });
+    })
   }
 }
